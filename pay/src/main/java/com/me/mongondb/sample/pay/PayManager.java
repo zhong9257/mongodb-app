ba846d742920ac3cf8da2f964ace3cb192d18116
zhongyi <zhongyi8307@163.com>
2017-01-03 12:07:50 +0800

    1.0.1 +创建pay项目

---
+++ pay/src/main/java/com/me/mongondb/sample/pay/PayManager.java
@@ -0,0 +1,16 @@
+package com.me.mongondb.sample.pay;
+
+public class PayManager {
+	
+	public String getOrderId(){
+		return null;
+	}
+	
+	public void transfer(double amt,String source,String dest){
+		
+	}
+	
+	public void prepareTransfer(double amt,String source,String dest){
+		
+	}
+}
